8d125a03e08c12b1042470ca5b31b6fe37297056
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-05-17 18:46:48 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-
+class Book
+    attr_reader :title, :author_name, :page_count, :genre
+  
+    def (title)
+      @title = title
+      @author_name = "Unknown"
+      @page_count = 0
+      @genre = "Unknown"
+    end
+  
+    def turn_page
+      puts "Flipping the page..."
+    end
+  end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+  
+    def (brand)
+      @brand = brand
+      @color = "Unknown"
+      @size = 0
+      @material = "Unknown"
+      @condition = "new"
+    end
+  
+    def cobble
+      self.condition = "new"
+      "Your shoe is as good as new!"
+    end
+  end
